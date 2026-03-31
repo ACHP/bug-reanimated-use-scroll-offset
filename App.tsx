e6bd6a24d0c5739714166d3fccd7b4328086a22c
Alexis CHAPPRON <bullrot-10@hotmail.fr>
2026-03-31 14:59:05 +0200

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatListProps, ScrollViewProps, StyleSheet, Text, View} from 'react-native';
+import Reanimated, {useAnimatedRef, useDerivedValue, useScrollOffset} from 'react-native-reanimated';
+import {useCallback} from "react";
+
 
 export default function App() {
+  const animatedRef = useAnimatedRef<Reanimated.FlatList>();
+  const scrollOffset = useScrollOffset(animatedRef);
+
+  const data = Array.from({length: 100}, (_, i)=> `item-${i}` );
+  const renderItem = useCallback<NonNullable<FlatListProps<string>["renderItem"]>>((info)=>{
+    return <Text>{info.item}</Text>
+  }, []);
+
+  useDerivedValue(()=>{
+    console.log(scrollOffset.value);
+    return 1;
+  })
+
+  const renderScrollComponent = useCallback((props: ScrollViewProps)=>{
+      return <CustomScrollComponent {...props}/>
+  }, []);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Reanimated.FlatList
+        data={data}
+        ref={animatedRef}
+        renderItem={renderItem}
+        renderScrollComponent={renderScrollComponent}
+      />
     </View>
   );
 }
@@ -18,3 +42,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+const CustomScrollComponent = (props: ScrollViewProps) => (
+  <View>
+    <Reanimated.ScrollView {...props}/>
+  </View>
+)
